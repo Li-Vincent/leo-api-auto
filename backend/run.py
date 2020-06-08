b2a87e796d91fb92d8900447a9c7318c33ca7dbf
liwh9 <liwh9@lenovo.com>
2020-06-08 14:26:31 +0800

    modify Deploy

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,5 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True, host='0.0.0.0', port=8888)
     app.logger.info('start leo-api-auto-platform service')
